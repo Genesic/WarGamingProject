7ab864b045dae2c28b106f59046468861375930b
Genesic <genesic@chuhenweide-MacBook-Pro.local>
2016-02-19 10:20:33 +0800

    client modify

--- client/Rhythm/Assets/Resource/Script/GamePlay/GamePlayUI.cs
+++ client/Rhythm/Assets/Resource/Script/GamePlay/GamePlayUI.cs
@@ -25,9 +25,9 @@ public class GamePlayUI : MonoBehaviour
     public PreTempo preTempo;
     public PreTempo preClick;
     public GameObject touchPanel;
-    public PointDefClick pointDefClick;
     public AudioClip correctSE;
     public int hpMax;
+    public int rivalHpMax;
     public int hp;
     public int rivalHp;
 
@@ -78,120 +78,9 @@ public class GamePlayUI : MonoBehaviour
     public void updateRivalHp(int newHp)
     {
         rivalHp = newHp;
-        float percent = (float)rivalHp / (float)hpMax;
+        float percent = (float)rivalHp / (float)rivalHpMax;
         float width = rivalHpMaxBar.rect.width;
         float height = rivalHpMaxBar.rect.height;
         rivalHpBar.sizeDelta = new Vector2(width * percent, height);
-    }
-
-    // 開始攻擊
-    public void startAttack()
-    {
-        touchPanel.SetActive(true);
-        StartCoroutine(atkAnime());
-    }
-
-    private int atk_round = 0;
-    public int atkMaxRound = 4;
-    IEnumerator atkAnime()
-    {
-        // 初始化
-        JSONNode res = new JSONClass();
-        var pointAtkClick = touchPanel.GetComponent<PointAtkClick>();
-        side.text = "ATTACK";
-        side.color = Color.blue;
-        pointAtkClick.init(atkMaxRound);
-        
-        yield return new WaitForSeconds(MainManager.beat/2);
-        // 攻擊處理
-        for (atk_round = 1; atk_round <= atkMaxRound; atk_round++)
-        {
-            roundTimes.text = atk_round.ToString();
-            roundTimes.color = Color.blue;
-            atkTempo.SetTrigger(MainManager.getTrigger());
-            yield return new WaitForSeconds(MainManager.beat/2);
-            string idx = atk_round.ToString();
-            
-            if( pointAtkClick.atkRes[atk_round] == ClickRes.None )
-            {   // 如果到最後都沒按的話就當miss處理
-                preTempo.updateResult(ClickRes.Miss);
-                pointAtkClick.atkRes[atk_round] = ClickRes.Miss;
-            }
-            yield return new WaitForSeconds(MainManager.beat/2);
-            res[idx][0] = ((int)pointAtkClick.atkRes[atk_round]).ToString();
-            res[idx][1] = pointAtkClick.atkPos[atk_round].x.ToString();
-            res[idx][2] = pointAtkClick.atkPos[atk_round].y.ToString();
-        }
-
-        atk_round = 0;
-        roundTimes.color = Color.clear;
-        MainManager.socket.SendData("attack " + res.ToString());
-        touchPanel.SetActive(false);
-    }
-
-    public int getAtkRound() { return atk_round; }
-
-    // 開始防禦
-    private int def_round;
-    public int getDefRound() { return def_round; }
-    private ClickRes defForAtkRes;
-    public ClickRes getDefForAtkRes() { return defForAtkRes; }
-    public void startDefend(JSONNode data)
-    {
-        StartCoroutine(defAnime(data));
-    }
-
-    IEnumerator defAnime(JSONNode data)
-    {
-        // 初始化
-        JSONNode resJson = new JSONArray();
-        RectTransform preClickRT = preClick.gameObject.GetComponent<RectTransform>();
-        Animator preClickAnime = preClick.gameObject.GetComponent<Animator>();
-        side.text = "DEFEND";
-        side.color = Color.red;
-        pointDefClick.init(atkMaxRound);
-        
-        // 開始處理防禦
-        for (def_round = 1; def_round <= atkMaxRound; def_round++)
-        {
-            roundTimes.text = def_round.ToString();
-            roundTimes.color = Color.red;
-            string idx = def_round.ToString();
-            Debug.Log(data[idx][0]);
-            defForAtkRes = MainManager.intToClickRes(int.Parse(data[idx][0]));
-            float x = float.Parse(data[idx][1]);
-            float y = float.Parse(data[idx][2]);
-            if (PreTempo.defList.ContainsKey(defForAtkRes))
-            {
-                preClickRT.position = new Vector2(x, y);
-                preClick.gameObject.SetActive(true);
-                preClickAnime.SetTrigger(MainManager.getTrigger());
-            }
-            else
-            {
-                preClick.gameObject.SetActive(false);
-            }
-            yield return new WaitForSeconds(MainManager.beat/2);
-            if( pointDefClick.defRes[def_round] == ClickRes.None )
-            {   // 如果到最後都沒按的話就當miss處理
-                preClick.updateResult(ClickRes.Miss);
-                pointDefClick.defRes[def_round] = ClickRes.Miss;
-            }
-            yield return new WaitForSeconds(MainManager.beat/2);
-            // 取得防禦結果
-            ClickRes defRes = pointDefClick.defRes[def_round];
-            int clickRes = (int)defRes;
-            // Debug.Log("defRes:"+defRes+" def_round:"+def_round+" clickRes:"+clickRes);
-            resJson[def_round-1] = clickRes.ToString();
-        }
-
-        // 清除防禦狀態
-        roundTimes.color = Color.clear;
-        def_round = 0;
-        defForAtkRes = ClickRes.None;
-        preClick.gameObject.SetActive(false);
-
-        // 送出防禦結果
-        MainManager.socket.SendData("defend_res " + resJson.ToString());
     }
 }
--- client/Rhythm/Assets/Resource/Script/Manager/Command.cs
+++ client/Rhythm/Assets/Resource/Script/Manager/Command.cs
@@ -4,66 +4,71 @@ using System.Collections.Generic;
 using SimpleJSON;
 using MiniJSON;
 
-public class Command : MonoBehaviour {
+public class Command : MonoBehaviour
+{
     public GameFlow gameFlow;
     public StartSelect startSelect;
     public GamePlayUI gamePlayUI;
     public TempoPlay tempoPlay;
-    
+
     public List<string> socketQueue;
-    
+
     public void init()
     {
         gameFlow = gameObject.GetComponent<GameFlow>();
     }
-    
+
     void FixedUpdate()
     {
-        while( socketQueue.Count > 0 ){
-            Debug.Log("GetLine:"+socketQueue[0]);
+        while (socketQueue.Count > 0)
+        {
+            Debug.Log("GetLine:" + socketQueue[0]);
             getSocket(socketQueue[0]);
             socketQueue.RemoveAt(0);
         }
     }
-    
+
     public void getSocket(string line)
     {
         string[] strs = line.Split(' ');
         string cmd = strs[0];
-        string jsons = ( strs.Length > 1)? strs[1] : "[]";
-        if( cmd == "ping"){
+        string jsons = (strs.Length > 1) ? strs[1] : "[]";
+        if (cmd == "ping")
+        {
             socket_ping();
-        } else if( cmd == "login"){
-            var args = JSON.Parse(jsons);
-            int res = int.Parse(args[0]);
-            socket_login(res);
-        } else if( cmd == "select_role"){
+        }
+        else if (cmd == "login")
+        {
+            socket_login();
+        }
+        else if (cmd == "select_role")
+        {
             var args = JSON.Parse(jsons);
             int res = int.Parse(args[0]);
             socket_select_role(res);
-        } else if( cmd == "match"){
+        }
+        else if (cmd == "match")
+        {
             var args = JSON.Parse(jsons);
-            int res = int.Parse(args[0]);
-            if( res == 1 )
-                socket_match();
-        } else if( cmd == "start_game"){
+            socket_match(args);
+        }
+        else if (cmd == "start_game")
+        {
             var args = JSON.Parse(jsons);
             socket_start_game(args);
-        } else if( cmd == "round_set"){
-            var args = JSON.Parse(jsons);
-            socket_round_set(args);
-        } else if( cmd == "round_start"){
-            socket_round_start();
-        } else if( cmd == "defend_start"){ // v1.0
-            var args = JSON.Parse(jsons);
-            socket_defend_start(args);
-        } else if( cmd == "def"){ // v2.0
+        }
+        else if (cmd == "def")
+        { // v2.0
             var args = JSON.Parse(jsons);
             socket_def(args);
-        } else if( cmd == "sync"){
+        }
+        else if (cmd == "sync")
+        {
             var args = JSON.Parse(jsons);
             socket_sync(args);
-        } else if( cmd == "end_game"){
+        }
+        else if (cmd == "end_game")
+        {
             socket_end_game();
         }
     }
@@ -71,101 +76,83 @@ public class Command : MonoBehaviour {
     {
         MainManager.socket.SendData("pong");
     }
-    public void socket_login(int res)
+    public void socket_login()
     {
-        if(res == 1){            
-            Dictionary<string, float> tmp = gameFlow.getGameSet();
-            string gameSet = Json.Serialize(tmp);
-            MainManager.socket.SendData("game_set "+gameSet);
-            // 開啟選擇角色面板
-            startSelect.selectRolePanel.SetActive(true);
-        } else {
-            // StartCoroutine(reLogin());
-        }
+        //Dictionary<string, float> tmp = gameFlow.getGameSet();
+        //string gameSet = Json.Serialize(tmp);
+        // MainManager.socket.SendData("game_set " + gameSet);
+        // 開啟選擇角色面板
+        startSelect.selectRolePanel.SetActive(true);
     }
-    
-     IEnumerator reLogin()
-     {
-         yield return new WaitForSeconds(3);
-         MainManager.socket.connectToServer();
-     }
-     
-     public void socket_select_role(int res)
-     {
-         if( res >= 1 ){
+
+    IEnumerator reLogin()
+    {
+        yield return new WaitForSeconds(3);
+        MainManager.socket.connectToServer();
+    }
+
+    public void socket_select_role(int res)
+    {
+        if (res >= 1)
+        {
             // 選擇角色成功後開啟match面板
             startSelect.login.SetActive(false);
             startSelect.selectRolePanel.SetActive(false);
             startSelect.match.SetActive(true);
             MainManager.socket.SendData("match");
-         }
-     }
-     
-     public void socket_match()
-     {
-         gamePlayUI.updateCombo(0);
-         startSelect.match.SetActive(false);
-         gamePlayUI.gameObject.SetActive(true);
-         MainManager.socket.SendData("ready");         
-     }
-     
-     public void socket_start_game(JSONNode data)
-     {
-         int maxHp = int.Parse(data["max_hp"]);
-         int role = int.Parse(data["role"]);
-         int rivalRole = int.Parse(data["rival_role"]);
-         Debug.Log("maxHp:"+maxHp+" role:"+role+" rivalRole:"+rivalRole);
-         gamePlayUI.hpMax = maxHp;
-         gamePlayUI.updateChName(role);
-         gamePlayUI.updateRivalName(rivalRole);
-         MainManager.socket.SendData("round_set");
-     }
-     
-     public void socket_round_set(JSONNode data)
-     {
-         int hp = int.Parse(data["hp"]);
-         int rival_hp = int.Parse(data["rival_hp"]);
-         int max_atk = int.Parse(data["max_atk"]);
-         int bpm = int.Parse(data["bpm"]);
-         int tempo = int.Parse(data["tempo"]);
-         
-         gamePlayUI.updateHp(hp);
-         gamePlayUI.updateRivalHp(rival_hp);
-         gamePlayUI.atkMaxRound = max_atk;
-         MainManager.updateBpm(bpm, tempo);
-         MainManager.socket.SendData("round_ready");
-     }
-     
-     public void socket_round_start()
-     {
-         tempoPlay.init();
-         tempoPlay.startTempo();
-         // gamePlayUI.startAttack();
-     }
-     
-     public void socket_defend_start(JSONNode data)
-     {
-         gamePlayUI.startDefend(data);             
-     }
-     
-     public void socket_def(JSONNode data)
-     {
-         int res = int.Parse(data[0]);
-         float x = float.Parse(data[1]);
-         float y = float.Parse(data[2]);         
-         tempoPlay.pushDefQueue(res, x, y);
-     }
-     
-     public void socket_sync(JSONNode data)
-     {
-         int hp = int.Parse(data["hp"]);
-         int rival_hp = int.Parse(data["rival_hp"]);
-         gamePlayUI.updateHp(hp);
-         gamePlayUI.updateRivalHp(rival_hp);
-     }
-     
-     public void socket_end_game()
-     {
-         MainManager.updateEndGame(true);
-     }
+        }
+    }
+
+    public void socket_match(JSONNode data)
+    {
+        int res = int.Parse(data["res"]);
+        if (res == 1)
+        {
+            int hp = int.Parse(data["hp"]);
+            int rivalHp = int.Parse(data["rival_hp"]);  
+            int role = int.Parse(data["role"]);
+            int rivalRole = int.Parse(data["rival_role"]);
+            // 初始化
+            gamePlayUI.updateCombo(0);
+            startSelect.match.SetActive(false);
+            gamePlayUI.gameObject.SetActive(true);
+            
+            // 我方初始化
+            gamePlayUI.hpMax = hp;
+            gamePlayUI.updateChName(role);
+            // 敵方初始化
+            gamePlayUI.rivalHpMax = rivalHp;
+            gamePlayUI.updateRivalName(rivalRole);
+            
+            MainManager.socket.SendData("ready");
+        }
+    }
+
+    public void socket_start_game(JSONNode data)
+    {
+        tempoPlay.init();
+        tempoPlay.startTempo();
+        MainManager.socket.SendData("round_set");
+    }
+
+    public void socket_def(JSONNode data)
+    {
+        int res = int.Parse(data[0]);
+        float x = float.Parse(data[1]);
+        float y = float.Parse(data[2]);
+        tempoPlay.pushDefQueue(res, x, y);
+    }
+
+    public void socket_sync(JSONNode data)
+    {
+        int hp = int.Parse(data["hp"]);
+        int rival_hp = int.Parse(data["rival_hp"]);
+        gamePlayUI.updateHp(hp);
+        gamePlayUI.updateRivalHp(rival_hp);
+    }
+
+    public void socket_end_game()
+    {
+        MainManager.updateEndGame(true);
+    }
 }
